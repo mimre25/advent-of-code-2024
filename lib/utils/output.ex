1786eb4ee152f29393b94e9d9ee496fe64395099
Martin Imre <martinimre25@gmail.com>
2024-12-21 14:31:14 +0100

    feat(output): Add function to print tuple matrix

--- lib/utils/output.ex
+++ lib/utils/output.ex
@@ -14,4 +14,27 @@ defmodule Output do
         Enum.reduce(row, "", fn {_, c}, acc -> acc <> c end)
     end)
   end
+
+  @spec tuple_matrix_to_str(Types.tuple_matrix(String.t())) :: String.t()
+  @doc """
+    Turns a tuple matrix into a string for printing
+  """
+  def tuple_matrix_to_str(matrix) do
+    {res, _} =
+      matrix
+      |> Enum.sort()
+      |> Enum.reduce({"", 0}, fn {{i, _}, e}, {acc, acc_i} ->
+        acc =
+          if i > acc_i do
+            acc <> "\n"
+          else
+            acc
+          end
+
+        acc = acc <> e
+        {acc, i}
+      end)
+
+    res
+  end
 end
